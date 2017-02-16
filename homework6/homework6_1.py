7a05b9487df0f8c6a7da16a160829b0841ea9e45
Антон Югов <i@ayugov.ru>
2017-02-16 21:29:03 +0300

    Добавил первую часть ДЗ №6

---
+++ homework6/homework6_1.py
@@ -0,0 +1,16 @@
+# Написать декоратор, который отменяет выполнение функции и пишет: ИМЯ_ФУНКЦИИ не будет вызвана
+
+def antidef(my_function):
+    def delfunc(*args):
+        print(str(my_function.__name__), 'не будет вызвана')
+
+    return delfunc
+
+
+@antidef
+def megafunc():
+    print('test')
+
+
+if __name__ == '__main__':
+    megafunc()
